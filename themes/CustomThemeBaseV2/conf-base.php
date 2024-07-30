91361589a150cb411585c147d515322bc213775a
Cunha <everson.c.junior@gmail.com>
2024-07-30 11:37:16 -0300

    adiciona projeto customizado p prefeitura Triunfo

--- themes/CustomThemeBaseV2/conf-base.php
+++ themes/CustomThemeBaseV2/conf-base.php
@@ -1,5 +1,12 @@
 <?php
 return [
-    'logo.color1' => env('LOGO_COLOR1', '#666'),
-    'logo.color2' => env('LOGO_COLOR1', '#999'),
+
+    /**App Site */
+    'app.siteName' => 'Mapas Culturais',
+    'app.siteDescription' => 'O Mapas Culturais é uma plataforma colaborativa que reúne informações sobre agentes, espaços, eventos, projetos culturais e oportunidades',
+
+    /**Logo */
+    'logo.title' => 'Triunfo',
+    'logo.subtitle' => 'Prefeitura',
+    'logo.image' => 'img/logo.png',
 ];
